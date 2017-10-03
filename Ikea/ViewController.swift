1ffed5ef0fea907b176f6b841f52772e459c23c9
Michael Chapman <submariner100@gmail.com>
2017-10-03 10:31:36 +0100

    second commit

--- Ikea/ViewController.swift
+++ Ikea/ViewController.swift
@@ -16,18 +16,47 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
 	@IBOutlet weak var itemsCollectionView: UICollectionView!
 	
 	let configuration = ARWorldTrackingConfiguration()
+	var selectedItem: String?
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 	
 		self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+		self.configuration.planeDetection = .horizontal
 		self.sceneView.session.run(configuration)
 		self.itemsCollectionView.dataSource = self
 		self.itemsCollectionView.delegate = self
+		self.registerGestureRecognizers()
 		
-	
 	}
-
+	
+	func registerGestureRecognizers() {
+		let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+		self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+	}
+	
+	@objc func tapped(sender: UITapGestureRecognizer) {
+		let sceneView = sender.view as! ARSCNView
+		let taplocation = sender.location(in: sceneView)
+		let hitTest = sceneView.hitTest(taplocation, types: .existingPlaneUsingExtent)
+		if !hitTest.isEmpty {
+			self.addItem(hitTestResult: hitTest.first!)
+			
+		}
+	}
+	
+	func addItem(hitTestResult: ARHitTestResult) {
+		if let selectedItem = self.selectedItem {
+			let scene = SCNScene(named: "Models.scnassets/\(selectedItem).scn")
+			let node = (scene?.rootNode.childNode(withName: selectedItem, recursively: false))!
+			let transform = hitTestResult.worldTransform
+			let thirdColumn = transform.columns.3
+			node.position = SCNVector3(thirdColumn.x, thirdColumn.y, thirdColumn.z)
+			self.sceneView.scene.rootNode.addChildNode(node)
+		}
+	}
+	
+	
 	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
 		return itemsArray.count
 	}
@@ -36,11 +65,11 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
 		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Item", for: indexPath) as! ItemCell
 		cell.itemLabel.text = self.itemsArray[indexPath.row]
 		return cell
-		
 	}
 	
 	func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
 		let cell = collectionView.cellForItem(at: indexPath)
+		self.selectedItem = itemsArray[indexPath.row]
 		cell?.backgroundColor = UIColor.green
 	}
 	
@@ -49,16 +78,14 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
 		cell?.backgroundColor = UIColor.orange
 	}
 	
-	func centerPivot(for node: SCNNode) {
-		let min = node.boundingBox.min
-		let max = node.boundingBox.max
-		node.pivot = SCNMatrix4MakeTranslation(
-			min.x + (max.x - min.x)/2,
-			min.y + (max.y - min.y)/2,
-			min.z + (max.z - min.z)/2
-		)
-	}
-
-
+//	func centerPivot(for node: SCNNode) {
+//		let min = node.boundingBox.min
+//		let max = node.boundingBox.max
+//		node.pivot = SCNMatrix4MakeTranslation(
+//			min.x + (max.x - min.x)/2,
+//			min.y + (max.y - min.y)/2,
+//			min.z + (max.z - min.z)/2
+//		)
+//	}
 }
 
